b2b882a2347e050d3bdc7dc5466caf082008372c
Arthur Weale <ajayrossy340@gmail.com>
2025-06-11 03:13:33 +0200

    Complete city model

---
+++ js/models/cityModel.js
@@ -0,0 +1,13 @@
+export class CityModel {
+  constructor() {
+    this.currentCity = null;
+  }
+  
+  setCurrentCity(city) {
+    this.currentCity = city;
+  }
+  
+  getCurrentCity() {
+    return this.currentCity;
+  }
+}
