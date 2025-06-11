127da8fc2ca34e0cc319d30d85a21fc5d613bdd4
Arthur Weale <ajayrossy340@gmail.com>
2025-06-11 03:14:06 +0200

    Created model for recent

---
+++ js/models/recentModel.js
@@ -0,0 +1,22 @@
+export class RecentModel {
+  constructor() {
+    this.recentSearches = [];
+  }
+  
+  getRecentSearches() {
+    return this.recentSearches;
+  }
+  
+  saveRecentSearch(city) {
+    // Remove if already exists
+    this.recentSearches = this.recentSearches.filter(search => search.id !== city.id);
+    
+    // Add to beginning
+    this.recentSearches.unshift(city);
+    
+    // Limit to 5 items
+    if (this.recentSearches.length > 5) {
+      this.recentSearches.pop();
+    }
+  }
+}
