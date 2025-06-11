33afde758acf9c4c7f2e6e4c7f93de3b0a2b1482
Arthur Weale <ajayrossy340@gmail.com>
2025-06-11 03:13:49 +0200

    Implemented favorite model

---
+++ js/models/favoriteModel.js
@@ -0,0 +1,24 @@
+export class FavoriteModel {
+  constructor() {
+    this.favorites = [];
+  }
+  
+  getFavorites() {
+    return this.favorites;
+  }
+  
+  addFavorite(city) {
+    // Prevent duplicates
+    if (!this.favorites.some(fav => fav.id === city.id)) {
+      this.favorites.push(city);
+    }
+  }
+  
+  removeFavorite(cityId) {
+    this.favorites = this.favorites.filter(fav => fav.id !== cityId);
+  }
+  
+  isFavorite(cityId) {
+    return this.favorites.some(fav => fav.id === cityId);
+  }
+}
